94729bea99d6aa50e089b6e88a740ab9b3459017
Nitesh Kohli <celestialstark007@gmail.com>
2025-02-15 13:03:29 +0000

    comment added

--- Solid principles/Interface_segregation_principle/Main.java
+++ Solid principles/Interface_segregation_principle/Main.java
@@ -1,5 +1,8 @@
 package Interface_segregation_principle;
 
+//A class should not be forced to implement interfaces it does not use.
+//This means that large interfaces should be split into smaller, more specific ones to avoid unnecessary dependencies.
+
 public class Main {
     public static void main(String[] args) {
         Chef chef = new Chef();
--- Solid principles/Liskov_substitution_principle/Main.java
+++ Solid principles/Liskov_substitution_principle/Main.java
@@ -1,5 +1,9 @@
 package Liskov_substitution_principle;
 
+/*
+ "Objects of a derived class should be able to replace objects of the base class without affecting the correctness of the program."
+This means that a subclass should extend the behavior of a parent class without changing its expected behavior.
+ */
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
